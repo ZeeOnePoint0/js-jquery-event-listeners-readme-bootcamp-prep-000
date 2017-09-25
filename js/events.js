98d1c3e6b09db3900084772f8983d3fee4930cd7
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-25 20:45:56 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,6 +20,16 @@ function frameIt() {
 
 }
 
+function submitIt() {
+
+  $('form').on("submit", function() {
+
+    alert("Your form is going to be submitted now.")
+    
+  })
+  
+}
+
 $(document).ready(function(){
 
 // call functions here
